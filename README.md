da2cf89a17bec74d607df038d5a6142ad115f6ab
username_anjastabuni <reblytabuni2004@gmail.com>
2025-03-06 14:34:36 +0700

    membuat rute crud mahasiswa dan middleware

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# d-tree-c4.5
+# Belajar algoritma decision tree c4.5
+
+## hari Pertama
+
+✅ Install node & express
+✅ Buat Server dasar
+✅ Coba jalankan server dan akses di browser
+
+## hari Kedua
+
+✅ Buat Rute CRUD untuk mahasiswa
+✅ Coba API pakai extensi visual studi code Thunder client
+✅ Coba jalankan server dan akses di browser
+✅ Tambahkan middleware logging
--- server.js
+++ server.js
@@ -4,10 +4,50 @@ const port = 3000;
 
 // middleware
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 
 // routes
+// home
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Selamat datang di sistem prediksi kelulusan mahasiswa.");
+});
+
+// get untuk ambil data mahasiswa
+app.get("/mahasiswa", (req, res) => {
+  res.json([
+    {
+      npm: "18411017",
+      nama: "Rebly Megib Tabuni",
+      ipk: [3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.1, 4.2],
+    },
+    {
+      npm: "18411018",
+      nama: "Jelky Walia",
+      ipk: [3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.1, 4.2],
+    },
+  ]);
+});
+
+// post untuk menambah data mahasiswa
+app.post("/mahasiswa", (req, res) => {
+  const data = req.body;
+  res.status(201).json({ message: "Mahasiswa ditambahkan!", data });
+});
+
+// put untuk mengupdate mahasiswa by npm
+app.put("/mahasiswa/:npm", (req, res) => {
+  const { npm } = req.params;
+  const updatedData = req.body;
+  res.json({ message: `Data mahasiswa ${npm} diperbaruhi`, updatedData });
+});
+
+// delete untuk menghapus mahasiswa
+app.delete("/mahasiswa/:npm", (req, res) => {
+  const { npm } = req.params;
+  res.json({ message: `mahasiswa dengan ${npm} dihapus` });
 });
 
 app.listen(port, () => {
